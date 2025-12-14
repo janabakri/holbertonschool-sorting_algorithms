2f8701bfc8ed38e80c0b7938136c05bad77ede5c
janabakri <bkeyh382@gmail.com>
2025-12-14 10:08:08 +0000

    Add Bubble Sort algorithm, Big O notation, and header file

--- sort.h
+++ sort.h
@@ -6,9 +6,9 @@
 /* Data structure for doubly linked list */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Function prototypes */
